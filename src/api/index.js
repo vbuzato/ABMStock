b3c25466379fb765afd0afb611798b0621f5858a
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-17 04:56:55 -0300

    CRUD api complete - src/api Update and Delete functions done

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const idCrudCrud = '83e47f77730c4ba6add4ecca584500c3';
+const idCrudCrud = '54d6a8e33a8c48efab4114a141a1ce76';
 const DBNname = 'Stock';
 
 export async function insertProduct(body) {
@@ -10,7 +10,7 @@ export async function insertProduct(body) {
   const fetchResult = await fetch(`/${idCrudCrud}/${DBNname}`, options)
     .then((newUser) => newUser.json())
     .catch((err) => (err.message));
-  // console.log(fetchResult);
+
   return fetchResult;
 }
 
@@ -30,6 +30,31 @@ export async function getItemById(id) {
   const options = {
     headers: { 'content-type': 'application/json' },
     method: 'GET',
+  };
+  const itemById = await fetch(`/${idCrudCrud}/${DBNname}/${id}`, options)
+    .then((data) => data.json())
+    .catch((err) => (err.message));
+
+  return itemById;
+}
+
+export async function updateItemById(id, body) {
+  const options = {
+    headers: { 'content-type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify(body),
+  };
+  const fetchResult = await fetch(`/${idCrudCrud}/${DBNname}/${id}`, options)
+    .then((newUser) => newUser.json())
+    .catch((err) => (err.message));
+
+  return fetchResult;
+}
+
+export async function deleteItemById(id) {
+  const options = {
+    headers: { 'content-type': 'application/json' },
+    method: 'DELETE',
   };
   const itemById = await fetch(`/${idCrudCrud}/${DBNname}/${id}`, options)
     .then((data) => data.json())
